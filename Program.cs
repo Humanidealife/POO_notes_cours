5ef7ffea8145c9a4567ad016072f4e41183b13ea
qianwang <wangqianlf@live.com>
2024-03-02 21:31:09 +0100

    premier commit création et ajout gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_notes_cours // Remplacez MonNamespace par le nom de votre namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
